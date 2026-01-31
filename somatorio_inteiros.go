e191c5386542a5aaa441fce0795357645d961909
f3rnandesov <pdrofrnandes@proton.me>
2026-01-31 17:41:56 -0300

    test

---
+++ somatorio_inteiros.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var N int
+	fmt.Print("Digite um número inteiro: ")
+	_, err := fmt.Scan(&N)
+
+	if err != nil || N <= 0{
+		fmt.Println("Por favor, digite número inteiro positivo.")
+		return 
+	}
+
+	conta := N * (N + 1) / 2
+	fmt.Printf("Resultado: %d\n", conta)
+	fmt.Println("Fórmula usada: N x (N + 1) / 2")
+
+
+}
